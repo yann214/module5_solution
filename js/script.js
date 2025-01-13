e6a86a49edb54253aff1877cbe75a619a2961699
yann214 <129801866+yann214@users.noreply.github.com>
2025-01-13 08:09:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -101,11 +101,9 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // Appeler la fonction chooseRandomCategory avec les données des catégories
       var chosenCategory = chooseRandomCategory(categories);
+      var chosenCategoryShortName = "'" + chosenCategory.short_name + "'";
 
-      // Extraire la propriété shortName de l'objet de catégorie choisi
-      var chosenCategoryShortName = chosenCategory.shortName;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -118,20 +116,16 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      //
-      var homeHtmlToInsertIntoMainPage = insertProperty(
-        homeHtml,
-        "randomCategoryShortName",
-        "'" + chosenCategoryShortName + "'" // Ajouter des guillemets simples
-      );
-      
+      var homeHtmlToInsert = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+
+
+       
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // Insérer le HTML dans la page principale
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content", homeHtmlToInsert);
 
 
     },
